f0c6a87f7c18e7e154652cd3eed8f27799dca397
Pramesti Hatta K <opam.qweiop@gmail.com>
2017-01-01 01:00:52 +0700

    modify setup.sh and add host on main.py

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ def predict():
 
     
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug = True)
--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
+echo "==================================================================="
 echo "Install all depedencies with pip"
 pip3 install -r requirements.txt
 echo "All depedencies has been installed"
+echo "==================================================================="
 
+echo "==================================================================="
 echo "Train our model"
 sudo mkdir model
 python3 titanic.py
 echo "Done"
 echo "Model saved on /model"
+echo "==================================================================="
 
+
+echo "==================================================================="
 echo "We are ready!"
+echo "run python3 main.py"
+echo "==================================================================="
